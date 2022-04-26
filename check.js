2a98933e161ec9d2e7c9f67765101b11ccb14ccf
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2022-04-26 08:09:50 -0500

    Update check.js

--- check.js
+++ check.js
@@ -1,5 +1,6 @@
 if (window.location.href.includes("innerturtle123")==false){
     document.documentElement.innerHTML = "And you thought I woulnt notice... smh... also heber please delete your copy of my repl before more people fork it...";
+    document.write("And you thought I woulnt notice... smh... also heber please delete your copy of my repl before more people fork it...");
   setTimeout(() => {
     window.location.assign("https://cg.innerturtle123.repl.co/");
   }, 5000);
