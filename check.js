585d97edb33b5a3275d3da39f7ebe12aa7b3ff23
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2022-04-26 00:56:12 -0500

    Update check.js

--- check.js
+++ check.js
@@ -1,5 +1,5 @@
 let url = window.location.href
-if (url.includes("innerturtle123"));{
+if (url.includes("innerturtle123")==false){
     document.documentElement.innerHTML = "And you thought I woulnt notice... smh... also heber please delete your copy of my repl before more people fork it...";
   setTimeout(() => {
     window.location.assign("https://cg.innerturtle123.repl.co/");
