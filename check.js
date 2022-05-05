93972e233626f4baf1b21413caf1387aba14a5e3
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2022-05-05 15:03:13 -0500

    Update check.js

--- check.js
+++ check.js
@@ -1,7 +1,3 @@
-  document.head.insertAdjacentHTML("beforeend", `<audio src="https://lioncat6.github.io/cg/ow.mp3" id="my_audio" loop="loop"></audio>`);
-window.onload=function(){
-      document.getElementById("my_audio").play();
-    };
 if (window.location.href.includes("innerturtle123")==false){
     window.location.assign("https://games.innerturtle123.repl.co/redirect/");
   }
