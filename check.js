eab2170198c9d0842bdf1cd2ecd7893ee8631232
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2022-06-01 08:24:24 -0500

    Update check.js

--- check.js
+++ check.js
@@ -3,5 +3,5 @@ if (window.location.href.includes("innerturtle123")==false){
     window.location.assign("https://games.innerturtle123.repl.co/redirect/");
 
   };
-const element = document.getElementById('bubbly');
+let element = document.getElementById('bubbly');
 element.remove();
