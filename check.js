6bfa7dcf9a444a94262d808cb4af8356d4dd678f
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2022-06-01 08:06:45 -0500

    Update check.js
    Added bubbly delete for pride month

--- check.js
+++ check.js
@@ -2,4 +2,6 @@ if (window.location.href.includes("innerturtle123")==false){
 
     window.location.assign("https://games.innerturtle123.repl.co/redirect/");
 
-  }
+  };
+const element = document.getElementById('bubbly');
+element.remove();
